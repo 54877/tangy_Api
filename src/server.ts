711e6ca47f15acce86a9750d3f65a70992763421
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-05-25 11:02:25 +0800

    測試db

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config(); // 有jwt驗證就需要這段
+dotenv.config();
 
 import app from "./app.js";
 import { query } from "./db/db.js";
-// 啟動伺服器
+
 const PORT = process.env.PORT || 3000;
-app.listen(3000, async () => {
-  console.log("server started");
+
+app.listen(PORT, async () => {
+  console.log("server started on", PORT);
 
   const result = await query("SELECT NOW()");
   console.log("DB connected:", result.rows);
