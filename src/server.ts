b21edafd467cdff698862b5c6352a3525fe06ba5
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-06-04 17:04:45 +0800

    env連接異常

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,9 @@ async function start() {
   try {
     await query("SELECT 1");
     console.log("DB connected");
+    console.log("RAW DATABASE_URL:");
+    console.log(process.env.DATABASE_URL);
+    console.log("TYPE:", typeof process.env.DATABASE_URL);
   } catch (err) {
     console.error("DB failed:", err);
   }
