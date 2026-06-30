f8769ab146ccfdc03d75fda631159ad5e9127b13
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-06-30 13:39:44 +0800

    修復extends 覆蓋型別問題

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -23,7 +23,9 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as any;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
