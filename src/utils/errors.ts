d20d2b2a606bfe0b892cebcaf661bb1ee4f1f5a7
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-07-01 14:11:26 +0800

    修改Err回傳值

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,10 +2,12 @@ import { Request, Response, NextFunction } from "express";
 
 export class AppError extends Error {
   statusCode: number;
+  field?: string;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number, field?: string) {
     super(message);
     this.statusCode = statusCode;
+    this.field = field;
 
     Object.setPrototypeOf(this, AppError.prototype);
   }
@@ -20,11 +22,13 @@ export const errorHandler = (
   const statusCode = err instanceof AppError ? err.statusCode : 500;
 
   const message = err instanceof Error ? err.message : "伺服器錯誤";
+  const errors =
+    err instanceof AppError && err.field
+      ? { [err.field]: message }
+      : { message };
 
   res.status(statusCode).json({
     state: false,
-    errors: {
-      message: message,
-    },
+    errors,
   });
 };
