d86db9a0d8241aafb80f65613d7c3d496ec1ea6c
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-07-29 17:01:35 +0800

    修改getme 回傳參數

--- src/controllers/profile_controllers.ts
+++ src/controllers/profile_controllers.ts
@@ -1,14 +1,33 @@
+import prisma from "../db/prisma";
 import { getUserByIdLogic } from "../services/profile_services";
 import { JwtAsyncFunction } from "../types/asyncType";
 
+// export const getUser: JwtAsyncFunction = async (req, res) => {
+//   const id = req.user?.id;
+
+//   const userDate = await getUserByIdLogic(id);
+
+//   res.status(200).json({
+//     userDate,
+//     message: "登入成功",
+//     state: true,
+//   });
+// };
+
 export const getUser: JwtAsyncFunction = async (req, res) => {
-  const id = req.user?.id;
+  try {
+    const result = await prisma.$queryRaw`SELECT NOW()`;
 
-  const userDate = await getUserByIdLogic(id);
+    res.status(200).json({
+      state: true,
+      result,
+    });
+  } catch (error) {
+    console.error(error);
 
-  res.status(200).json({
-    userDate,
-    message: "登入成功",
-    state: true,
-  });
+    res.status(500).json({
+      state: false,
+      error,
+    });
+  }
 };
